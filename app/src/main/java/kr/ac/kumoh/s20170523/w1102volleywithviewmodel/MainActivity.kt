99758bba70a3ce1d967c94b9476037207444d1c4
W110pVolley <gmlghd98@gmail.com>
2022-11-16 14:55:43 +0900

    스마트앱 프로그래밍 11주차 2, 3차시

--- app/src/main/java/kr/ac/kumoh/s20170523/w1102volleywithviewmodel/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20170523/w1102volleywithviewmodel/MainActivity.kt
@@ -2,10 +2,37 @@ package kr.ac.kumoh.s20170523.w1102volleywithviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import kr.ac.kumoh.s20170523.w1102volleywithviewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var model: SongViewModel
+    private var songs: Array<String>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        model = ViewModelProvider(this)[SongViewModel::class.java]
+        model.requestSong()
+
+        model.songs.observe(this,
+        Observer<ArrayList<String>>{
+            Toast.makeText(this,
+                model.songs.value.toString(),
+                Toast.LENGTH_LONG).show()
+            songs = model.songs.value?.toTypedArray()
+
+            binding.listSongs.adapter = ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                songs as Array<out String>)
+        })
+
     }
 }
